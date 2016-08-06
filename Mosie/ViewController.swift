882b2cbafc000fad437f727ad1eb439993e9e7d5
Avery Lamp <averylamp@gmail.com>
2016-08-06 15:56:48 -0400

    Initial Commit

---
+++ Mosie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mosie
+//
+//  Created by Avery Lamp on 8/6/16.
+//  Copyright © 2016 MemeMasters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
