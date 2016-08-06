37aef1a5d64aaf990984946f742c7e290d247389
Avery Lamp <averylamp@gmail.com>
2016-08-06 17:17:54 -0400

    youtube pod added

--- Podfile
+++ Podfile
@@ -6,5 +6,5 @@ target 'Mosie' do
     pod 'Alamofire', '~> 3.4'
     pod 'SwiftyJSON'
     pod 'MBProgressHUD', '~> 1.0.0'
-
+    pod "youtube-ios-player-helper", "~> 0.1.4"
 end
--- Pods/Target Support Files/Pods-Mosie/Pods-Mosie-frameworks.sh
+++ Pods/Target Support Files/Pods-Mosie/Pods-Mosie-frameworks.sh
@@ -87,9 +87,11 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/youtube-ios-player-helper/youtube_ios_player_helper.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/youtube-ios-player-helper/youtube_ios_player_helper.framework"
 fi
